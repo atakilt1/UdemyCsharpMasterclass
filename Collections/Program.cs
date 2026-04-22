e1ae1b4f7fe8ed1395c9ea5ae86c366e7f4b9d0f
atakilt1 <atakilt.send@gmail.com>
2026-04-22 17:41:29 +0300

    dictionaries part 2

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -11,10 +11,26 @@ namespace Collections
 
             employees.Add(101, "John Doe");
             employees.Add(102, "Bob Smith");
+            employees.Add(103, "Bob Smith");
+            employees.Add(104, "Flob Smith");
+            employees.Add(105, "Dob Smith");
 
             string name = employees[101];
 
             Console.WriteLine("Name: " + name);
+
+            //update data 
+            employees[102] = "Jane Smith";
+
+            //remove item
+            employees.Remove(105);
+
+            Console.WriteLine();
+            //iterating over a dict
+            foreach(KeyValuePair<int,string> employee in employees)
+            {
+                Console.WriteLine($"ID: {employee.Key}, Name: {employee.Value}");
+            }
 
             Console.ReadKey();
         }
