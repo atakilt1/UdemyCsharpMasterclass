e304951d3610ebd533598769d9c0b9301f95718a
atakilt1 <atakilt.send@gmail.com>
2026-04-22 17:25:20 +0300

    dictionaries part 1

---
+++ Collections/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> employees = new Dictionary<int, string>();
+
+            employees.Add(101, "John Doe");
+            employees.Add(102, "Bob Smith");
+
+            string name = employees[101];
+
+            Console.WriteLine("Name: " + name);
+
+            Console.ReadKey();
+        }
+    }
+}
