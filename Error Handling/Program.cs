4c13630b9cd8eb30abbe1d8a54e359ff3fc91225
atakilt1 <atakilt.send@gmail.com>
2026-04-23 18:21:51 +0300

    try catch

---
+++ Error Handling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Error_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            try
+            {
+                int num1 = 2;
+                Console.WriteLine("Enter a number?");
+                int num2 = int.Parse(Console.ReadLine());
+                result = num1 / num2;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message); 
+            }
+
+            Console.WriteLine("Result: " + result);
+            Console.ReadKey();
+        }
+    }
+}
